e4f361536b409d6df40aee15fc06a3ac42ea459f
samibyte <samibyte.web@gmail.com>
2025-12-03 17:41:36 +0600

    feat: add new skills

--- src/pages/Home/components/PopularSkills.jsx
+++ src/pages/Home/components/PopularSkills.jsx
@@ -1,13 +1,20 @@
+import Loader from "../../../components/Loader";
 import SkillCard from "../../../components/SkillCard";
 
 const PopularSkills = ({ skillsData }) => {
+  if (!skillsData) {
+    return <Loader />;
+  }
+
+  const latestSkills = skillsData.slice(0, 8);
+
   return (
     <div>
       <h2 className="text-4xl mb-6 md:text-5xl font-bold text-center poppins-font">
         Popular <span className="text-secondary">Skills</span>
       </h2>
       <div className="grid grid-cols-1 md:grid-cols-4 sm:grid-cols-2 gap-10 py-10 sm:py-20">
-        {skillsData?.map((skill) => (
+        {latestSkills?.map((skill) => (
           <SkillCard key={skill.skillId} skill={skill} />
         ))}
       </div>
